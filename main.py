b2deb5e8b3b03dbdd92f3f66c1ead65701580342
TrishinA <trishin.aoff@mail.ru>
2023-06-12 22:14:13 +0300

    feat: Сделал через flask

--- main.py
+++ main.py
@@ -1,45 +1,65 @@
 from mod_ray import Answer2Questions
 from tests import main_test
+from flask import Flask
+from flask import request
+from flask import abort
+from enum import Enum
 
-
+import logging
 import asyncio
 import os
 
-
-async def handler(reader, writer):
-    data = await reader.read(100)
-    message = data.decode()
-    addr = writer.get_extra_info('peername')
-
-    print(f"Received {message!r} from {addr!r}")
-
-    Answer2Questions.Answer2Questions.check_avalible_answer(message)
-
-
-    print(f"Send: {message!r}")
-    writer.write(data)
-    await writer.drain()
-
-    print("Close the connection")
-    writer.close()
-
-
-async def main():
-    host = os.environ.get('host') if os.environ.get('host') is not None else '127.0.0.1'
-
-    port = os.environ.get('port') if os.environ.get('port') is not None else 4000
-
-    server = await asyncio.start_server(
-        handler,
-        host,
-        port)
+class FieldMessage(str, Enum):
+    QUESTION = 'question' 
+    ANSWER = 'answer'
+    PARAMS = 'params'
     
-    print(f'Serving on {server.sockets[0].getsockname()}')
 
-    asyncio.create_task(main_test.main())
+TemplateRequest = {
+    FieldMessage.QUESTION: '',
+    FieldMessage.PARAMS: {}
+} 
 
-    async with server:
-        await server.serve_forever()
+TemplateReponse = {
+    FieldMessage.ANSWER: ''
+} 
+
+def create_logger(logger_name):
+    logger = logging.getLogger(str(logger_name))
+    logger.setLevel(logging.INFO)
+
+    file_logger = logging.FileHandler('service_log.txt')
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_logger.setFormatter(formatter)
+
+    logger.addHandler(file_logger)
+    return logger
+
+
+logger = create_logger("Service main thread")
+
+def create_error_response(field_with_error):
+    response = TemplateReponse
+    response[FieldMessage.ANSWER] = f'Field {field_with_error} NOT found!'
+    return response
+
+def main(port):
+    service = Flask('Service main thread')
+
+    @service.route('/get_answer', methods=['POST'])
+    def get_answer():
+        for field in TemplateRequest.keys():
+            if field not in request.json:
+                return create_error_response(field)
+            
+        response = TemplateReponse
+        response[FieldMessage.ANSWER] = Answer2Questions.Service(request.json[FieldMessage.QUESTION], FieldMessage.PARAMS).get_answer()
+        return response
+    logger.info(f'Server start!')
+    service.run(port=port, debug=False)
+
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main(port=25000)
--- mod_ray/Answer2Questions.py
+++ mod_ray/Answer2Questions.py
@@ -1,24 +1,47 @@
+import json
+from fuzzywuzzy import fuzz
 
-class ModuleLogicAutoAnswer:
-
-    def __init__(self) -> None:
-        pass
-
-    def __delattr__(self, __name: str) -> None:
-        pass
-
-
-class Answer2Questions:
-    Answer2Questions = {
+class Service:
+    Services_message = {
         'Give_me_operator': 'Connection with operator...',
-        'Give_me_path_to_join': '123.0.0.123'
+        'Give_me_path_to_join': '123.0.0.123',
+        'Start_message': 'Service started!',
+        'Service_fail':'Service raise error!'
     }
+    def __init__(self, request, params) -> None:
+        self.config = config['autoanswer']
+        self.accuracy = 80 if 'accuracy' not in params else params['accuracy']
+        self.request = request
 
-    @staticmethod
-    def check_avalible_answer(question: str):
-        if question in Answer2Questions.Answer2Questions.keys():
-            print(Answer2Questions.Answer2Questions[question])
+    def get_answer(self):
+        list_results = list()
+        for request_from_config in self.config:
+            list_results.append(int(fuzz.token_sort_ratio(request_from_config, self.request)))
+        
+        max_acc = max(list_results)
 
+        if max_acc < self.accuracy:
+            return f"I don't know answer on this question: {self.request}" 
+        else:
+            return self.get_answer_to_position(list_results.index(max_acc))
+
+    def get_answer_to_position(self, position):
+        count = 0
+        for answer in self.config:
+            if position == count:
+                return self.config[answer]
+            count +=1
+
+
+def get_config(path_to_config = r'C:\Users\Aleksey ^_^\Desktop\ModRay\ModRay\mod_ray\configs\a2q.ini'):
+    with open(path_to_config, 'r') as save:
+        config = json.load(save)
+        if config is not None:
+            return config
+        else:
+            raise FileNotFoundError
+    
+config = get_config()
 
 if __name__ == '__main__':
     pass
